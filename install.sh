39886660cebc6065ada7fac71ce3477dac54e91b
Onni Lampi <contact@onnilampi.fi>
2018-01-15 22:12:39 +0200

    Script version 0.5

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-sudo apt install git vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl https://onnilampi.fi/static/vimrc >> ~/.vimrc
-vim +PluginInstall +qall
+if [[ $EUID -ne 0 ]]
+then
+    if [git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim]
+    then
+        curl https://onnilampi.fi/static/vimrc >> ~/.vimrc
+        vim +PluginInstall +qall
+    else
+        echo "Vundle is already installed"
+        curl https://onnilampi.fi/static/vimrc >> ~/.vimrc
+        vim +PluginInstall +qall
+    fi
+
+else
+    echo "Running as root, now installing git, vim and zsh"
+    apt install git vim zsh
+    echo "Exiting..."
+    exit 1
+fi
