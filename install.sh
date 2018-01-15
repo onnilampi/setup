9d5a36ada0596ffa9c3199941675768f5a4e4042
Onni Lampi <contact@onnilampi.fi>
2018-01-16 01:12:11 +0200

    Improve installation script logic

--- install.sh
+++ install.sh
@@ -5,58 +5,37 @@ echo "See https://github.com/onnilampi/setup for more details"
 
 if [[ $EUID -ne 0 ]]
 then
-    if [ 'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim' ] 
+    bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo "Installing Vundle..."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    if [ $? == 0 ]
     then
-        bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-        echo "Installing .vimrc..."
-        curl -fsSL raw.githubusercontent.com/onnilampi/setup/master/.vimrc -o ~/.vimrc
-        if [ $? == 0 ]
-        then
-            echo ".vimrc installed!"
-        fi
-        echo "Installing vim plugins..."
-        vim +PluginInstall +qall
-        if [ $? == 0 ]
-        then
-            echo "Vim plugins installed!"
-        fi
-        echo "Installing .zshrc and custom zsh theme..."
-        curl -fsSL raw.githubusercontent.com/onnilampi/setup/master/.zshrc -o ~/.zshrc
-        if [ $? == 0 ]
-        then
-            echo ".zshrc installed!"
-        fi
-        curl -fsSL raw.githubusercontent.com/onnilampi/setup/master/omnez.zsh-theme -o ~/.oh-my-zsh/themes/omnez.zsh-theme
-        if [ $? == 0 ]
-        then
-            echo "Custom zsh theme installed!"
-        fi
+        echo "Installed Vundle"
     else
-        echo "Vundle is already installed"
-        bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-        echo "Installing .vimrc..."
-        curl -fsSL raw.githubusercontent.com/onnilampi/setup/master/.vimrc -o ~/.vimrc
-        if [ $? == 0 ]
-        then
-            echo ".vimrc installed!"
-        fi
-        echo "Installing vim plugins..."
-        vim +PluginInstall +qall
-        if [ $? == 0 ]
-        then
-            echo "Vim plugins installed!"
-        fi
-        echo "Installing .zshrc and custom zsh theme"
-        curl -fsSL raw.githubusercontent.com/onnilampi/setup/master/.zshrc -o ~/.zshrc
-        if [ $? == 0 ]
-        then
-            echo ".zshrc installed!"
-        fi
-        curl -fsSL raw.githubusercontent.com/onnilampi/setup/master/omnez.zsh-theme -o ~/.oh-my-zsh/themes/omnez.zsh-theme
-        if [ $? == 0 ]
-        then
-            echo "Custom zsh theme  installed!"
-        fi
+        echo "Vundle is already installed."
+    fi
+    echo "Installing .vimrc..."
+    curl -fsSL raw.githubusercontent.com/onnilampi/setup/master/.vimrc -o ~/.vimrc
+    if [ $? == 0 ]
+    then
+        echo ".vimrc installed!"
+    fi
+    echo "Installing vim plugins..."
+    vim +PluginInstall +qall
+    if [ $? == 0 ]
+    then
+        echo "Vim plugins installed!"
+    fi
+    echo "Installing .zshrc and custom zsh theme..."
+    curl -fsSL raw.githubusercontent.com/onnilampi/setup/master/.zshrc -o ~/.zshrc
+    if [ $? == 0 ]
+    then
+        echo ".zshrc installed!"
+    fi
+    curl -fsSL raw.githubusercontent.com/onnilampi/setup/master/omnez.zsh-theme -o ~/.oh-my-zsh/themes/omnez.zsh-theme
+    if [ $? == 0 ]
+    then
+        echo "Custom zsh theme installed!"
     fi
 else
     echo "Running as root, now installing git, vim and zsh"
